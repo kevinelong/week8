9c91d6902e8abe78f29563181eff239461980dc3
kevinelong <kevinelong@gmail.com>
2023-05-31 07:58:12 -0700

    day

---
+++ exercises/return_the_day.js
@@ -0,0 +1,56 @@
+function whatday(num) { 
+
+    if(num < 1 || num > 7){
+        return "Wrong, please enter a number between 1 and 7"
+    }
+
+    const DAY_LIST = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+      
+    return DAY_LIST[num - 1];
+  
+    // const DAY_DICT=  {
+    //   1: "Sunday",
+    //   2: "Monday",
+    //   3: "Tuesday",
+    //   4: "Wednesday",
+    //   5: "Thursday",
+    //   6: "Friday",
+    //   7: "Saturday",
+    // };
+    
+    // return DAY_DICT[num];
+    
+    // put your code here
+  
+  //     if(num === 1) return "Sunday"
+  //     if(num === 2) return "Monday"
+  //     if(num === 3) return "Tuesday"
+  //     if(num === 4) return "Wednesday"
+  //     if(num === 5) return "Thursday"
+  //     if(num === 6) return "Friday"
+  //     if(num === 7) return "Saturday"
+    
+  //     return "Wrong, please enter a number between 1 and 7"
+  
+}
+
+const Test = require('@codewars/test-compat');
+
+describe( "whatday", function(){
+  it( "should work for the examples" , function(){
+    Test.assertEquals(whatday(1),'Sunday')
+    Test.assertEquals(whatday(2), 'Monday')
+    Test.assertEquals(whatday(3),'Tuesday')
+       Test.assertEquals(whatday(8),  'Wrong, please enter a number between 1 and 7')
+    Test.assertEquals(whatday(20),  'Wrong, please enter a number between 1 and 7')
+
+      });
+});
