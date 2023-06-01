f4c524b9bb53d9463136063bb5c5c5247ae61a12
kevinelong <kevinelong@gmail.com>
2023-06-01 07:43:05 -0700

    mario solutions

--- demos/demo4mario.js
+++ demos/demo4mario.js
@@ -16,3 +16,46 @@ Write functions to output each of these to N rows.
  #####
 #######
 */
+
+// TODO: Your code here
+
+
+/* SOLUTION SPOILERS BELOW
+
+
+
+function repeat(n,s){
+    let output = ""
+    for(let i =0; i < n; i++){
+        output += s;
+    }
+    return output;
+}
+
+function one(n){
+    let output = "";
+    for(let r = 0; r < n; r++){
+        output += repeat(r+1, "#") + "\n";
+    }
+    return output;
+}
+console.log(one(4))
+
+function two(n){
+    let output = "";
+    for(let r = 0; r < n; r++){
+        output += repeat(n - r - 1, " ") + repeat(r+1, "#") + "\n";
+    }
+    return output;
+}
+console.log(two(4))
+
+function three(n){
+    let output = "";
+    for(let r = 0; r < n; r++){
+        output += repeat(n - r - 1, " ") + repeat(r * 2 + 1, "#") + "\n";
+    }
+    return output;
+}
+console.log(three(4))
+*/
